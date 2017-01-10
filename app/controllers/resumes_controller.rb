5d05f2c64b7543ee206286b005fbd17ec28ad706
ronrazmi <ronrazmi@gmail.com>
2017-01-10 18:18:06 -0500

    routes and controller updated

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,7 +1,17 @@
 class ResumesController < ApplicationController
+
   def index
+
+  	@resumes = Unirest.get().body
+
+  	render 'index.html.erb'
   end
 
   def show
+
+  	@resume = resume.find(params[:id])
+
+  	render 'show.html.erb'
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'resumes/index'
 
-  get 'resumes/show'
+  get '/list' => 'resumes#index'
+  get '/list/:id' => 'resumes#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
