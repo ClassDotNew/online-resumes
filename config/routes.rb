5d267794442cadb7f07af5694742bf3e50d7dae5
Yvonne Ike <yviedev@gmail.com>
2017-01-12 19:03:19 -0500

    Add student id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get '/resumes' => 'resumes#index'
-  get '/list/:id' => 'resumes#show'
+  get '/resumes/:id' => 'resumes#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
